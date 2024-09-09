3790d77785471f53b05895f4ef7e26a6863c3811
rye141200 <126208685+rye141200@users.noreply.github.com>
2024-09-09 19:10:33 +0300

    Bootswatch theme, toastrs and routing

--- Client/src/app/nav/nav.component.ts
+++ Client/src/app/nav/nav.component.ts
@@ -2,12 +2,21 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 //import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [
+    FormsModule,
+    BsDropdownModule,
+    RouterLink,
+    RouterLinkActive,
+    TitleCasePipe,
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
@@ -17,18 +26,21 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 //! There is also reactive forms but we wont use it here
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
   model: any = {};
   login() {
     // console.log(this.model);
     this.accountService.login(this.model).subscribe({
-      next: (response) => {
-        console.log(response);
+      next: () => {
+        this.router.navigateByUrl('/members');
       },
-      error: (err) => console.log(err),
+      error: (err) => this.toastr.error(err.error),
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
